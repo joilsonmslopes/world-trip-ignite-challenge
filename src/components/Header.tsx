d11c1712348c22a303f823e137b6951790f7fd4e
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2023-03-20 22:05:00 -0300

    Improve Header component to use on Continent page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,34 @@
-import { Box, Flex, Image } from '@chakra-ui/react'
+import { Box, Flex, Image, Link as ChakraLink, Icon } from '@chakra-ui/react'
+import { FiChevronLeft } from 'react-icons/fi'
 
 import LogoImg from 'public/logo.png'
+import { FC } from 'react'
+import Link from 'next/link'
 
-export const Header = () => {
+interface HeaderProps {
+  hasButton?: boolean
+  linkTo?: string
+}
+
+export const Header: FC<HeaderProps> = ({ hasButton, linkTo }) => {
   return (
-    <Flex justify="center" py={27} px="10" bg="white">
+    <Flex justify="center" py={27} px="10" bg="white" position="relative">
+      {hasButton && (
+        <Box position="absolute" top="34px" left={10}>
+          <ChakraLink
+            as={Link}
+            href={linkTo}
+            passHref
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            w="8"
+            h="8"
+          >
+            <Icon as={FiChevronLeft} fontSize="lg" boxSize="auto" />
+          </ChakraLink>
+        </Box>
+      )}
       <Box
         w={{ base: '5.06rem', lg: '11.56rem' }}
         h={{ base: '1.25rem', lg: '2.875rem' }}
